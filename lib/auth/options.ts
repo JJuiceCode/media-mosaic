106cedb8596f8028e158500b13308fd76b6807d5
김주환 <juk1126@gmail.com>
2026-03-10 14:36:57 +0900

    소셜 로그인 client id 및 secret값 `.env` 에 추가후 수정

--- lib/auth/options.ts
+++ lib/auth/options.ts
@@ -3,21 +3,22 @@ import Google from "next-auth/providers/google";
 import Kakao from "next-auth/providers/kakao";
 import Naver from "next-auth/providers/naver";
 
-const googleClientId = process.env.AUTH_GOOGLE_ID;
-const googleClientSecret = process.env.AUTH_GOOGLE_SECRET;
-const naverClientId = process.env.AUTH_NAVER_ID;
-const naverClientSecret = process.env.AUTH_NAVER_SECRET;
-const kakaoClientId = process.env.AUTH_KAKAO_ID;
-const kakaoClientSecret = process.env.AUTH_KAKAO_SECRET;
+const googleClientId = process.env.AUTH_GOOGLE_CLIENT_ID;
+const googleClientSecret = process.env.AUTH_GOOGLE_CLIENT_SECRET;
+const naverClientId = process.env.AUTH_NAVER_CLIENT_ID;
+const naverClientSecret = process.env.AUTH_NAVER_CLIENT_SECRET;
+const kakaoClientId = process.env.AUTH_KAKAO_CLIENT_ID;
+const kakaoClientSecret = process.env.AUTH_KAKAO_CLIENT_SECRET;
+
 const authSecret = process.env.AUTH_SECRET;
 
 const missingEnv = [
-  ["AUTH_GOOGLE_ID", googleClientId],
-  ["AUTH_GOOGLE_SECRET", googleClientSecret],
-  ["AUTH_NAVER_ID", naverClientId],
-  ["AUTH_NAVER_SECRET", naverClientSecret],
-  ["AUTH_KAKAO_ID", kakaoClientId],
-  ["AUTH_KAKAO_SECRET", kakaoClientSecret],
+  ["AUTH_GOOGLE_CLIENT_ID", googleClientId],
+  ["AUTH_GOOGLE_CLIENT_SECRET", googleClientSecret],
+  ["AUTH_NAVER_CLIENT_ID", naverClientId],
+  ["AUTH_NAVER_CLIENT_SECRET", naverClientSecret],
+  ["AUTH_KAKAO_CLIENT_ID", kakaoClientId],
+  ["AUTH_KAKAO_CLIENT_SECRET", kakaoClientSecret],
   ["AUTH_SECRET", authSecret],
 ].filter(([, value]) => !value);
 
